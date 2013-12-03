280deae50c11413f13a84f1ae23d82837721cd0d
Kamalski <djkimozaki@gmail.com>
2013-12-03 11:24:53 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mushie::Application.config.secret_key_base = 'e88d943e521f3a9aa5c0d46c2dd00eab5887e855001ced2dad8bcb2f3f1f7f28feb97db176cf0a66ba4d0ffffdbdad5fc972f45753a9392cc304f03a3338fe7a'
